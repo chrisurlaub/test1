16760d35584df9dd77e78b1cf92223553ba6b764
Chris Lessenich <cl@in-telegence.net>
2016-03-10 14:13:28 +0100

    first commit

---
+++ api/controllers/AgentController.js
@@ -0,0 +1,11 @@
+/**
+ * AgentController
+ *
+ * @description :: Server-side logic for managing agents
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/CallcenterController.js
@@ -0,0 +1,11 @@
+/**
+ * CallcenterController
+ *
+ * @description :: Server-side logic for managing callcenters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
